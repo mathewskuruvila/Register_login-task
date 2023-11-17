99ae0b5a6192a2a3fdf2ad92d4d40e316526d36b
Mathews <mathewsulike20@gmail.com>
2023-11-17 16:46:51 +0530

    first commit

---
+++ Client/src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../src/Home.css'; 
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1 className="animated-heading">Hello World</h1>
+    </div>
+  );
+};
+
+export default Home;
